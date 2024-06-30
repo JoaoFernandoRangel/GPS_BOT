e0b2dfd34dac2853af1708e82584cfe5bd36c620
João Fernando Rangel <joaofrguimaraes1@gmail.com>
2024-06-30 14:05:32 -0300

    Finalizado falta BT
    Código de orientação e busca atualizado. Ainda necessita de teste em Campo.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <TinyGPSPlus.h>
-
 #include "math.h"
 
 #define Serial_Debug Serial
@@ -19,12 +18,14 @@ void faz_vetores(double x_zero, double y_zero, double x_1, double y_1, double x_
 double dot_prod(double x_vetor_0, double y_vetor_0, double x_vetor_1, double y_vetor_1, double angulo);
 double modulo(double x, double y);
 double to_rad(double angulo_grau);
+double to_ang(double angulo_rad);
 void anda_para_frente(int tempo);
 void anda_para_tras(int tempo);
 void gira_para_esquerda(int tempo);
 void gira_para_direita(int tempo);
-void ajusta_para(bool direita);
+void ajusta_para(bool direita, float tempo);
 void stop();
+void ajusta_angulo(double angulo0, double angulo1);
 
 //////////////Variáveis Declaradas////////////////////////
 
@@ -74,24 +75,77 @@ void loop()
     if (agora - zero >= 1000) // Executa a rotina a cada segundo.
     {
       pega_pontos();
-      faz_vetores(ponto0[0], ponto0[1], ponto1[0], ponto1[1], vec0[0], vec0[1]);                 // vetor de movimento
-      faz_vetores(ponto1[0], ponto1[1], ponto_goal[0], ponto_goal[1], vec_goal[0], vec_goal[1]); // vetor entre o ponto atual e o objetivo
-      dot_prod(vec0[0], vec0[1], vec_goal[0], vec_goal[1], angulo0);                             // retorna o angulo0 entre os dois vetores
-      if (calc_dist(ponto1[0], ponto1[1], ponto_goal[0], ponto_goal[1]) <= 2)                    // verifica se estamos a menos de 2 metros do alvo
+      if (calc_dist(ponto1[0], ponto1[1], ponto_goal[0], ponto_goal[1]) <= 2) // verifica se estamos a menos de 2 metros do alvo
       {
         stop();
         cond = false; // Para o loop uma vez que a distância é atingida.
-      }
-      if (angulo0 > angulo1)
-      {
-        ajusta_para(false); // Ajusta movimentação para direita
-      }
-      else
-      {
-        ajusta_para(true); // Ajusta movimentação para direita
-      }
+      } // Anda em linha reta por 2,5 segundos
+      faz_vetores(ponto0[0], ponto0[1], ponto1[0], ponto1[1], vec0[0], vec0[1]);                 // vetor de movimento
+      faz_vetores(ponto1[0], ponto1[1], ponto_goal[0], ponto_goal[1], vec_goal[0], vec_goal[1]); // vetor entre o ponto atual e o objetivo
+      dot_prod(vec0[0], vec0[1], vec_goal[0], vec_goal[1], angulo0);                             // retorna o angulo0 entre os dois vetores
+      ajusta_angulo(angulo0, angulo1);                                                           // Rotaciona o carrinho de acordo com o angulo adquirido
       angulo1 = angulo0;
       zero = agora;
+    }
+  }
+}
+
+void ajusta_angulo(double angulo0, double angulo1)
+{
+  double dif = to_ang(angulo1) - to_ang(angulo0);
+  if (abs(dif) >= 40)
+  {
+    if (dif < 0)
+    {
+      ajusta_para(true, 2.5); // Para direita por 2,5 segundos
+    }
+    else
+    {
+      ajusta_para(false, 2.5); // Ajusta para esquerda por 2,5 segundos
+    }
+  }
+  else if (abs(dif) < 40 && abs(dif) >= 30)
+  {
+    if (dif < 0)
+    {
+      ajusta_para(true, 1.5);
+    }
+    else
+    {
+      ajusta_para(false, 1.5);
+    }
+  }
+  else if (abs(dif) < 30 && abs(dif) >= 20)
+  {
+    if (dif < 0)
+    {
+      ajusta_para(true, 1);
+    }
+    else
+    {
+      ajusta_para(false, 1);
+    }
+  }
+  else if (abs(dif) < 20 && abs(dif) >= 10)
+  {
+    if (dif < 0)
+    {
+      ajusta_para(true, 0.5); // Para direita por 2,5 segundos
+    }
+    else
+    {
+      ajusta_para(false, 0.5); // Ajusta para esquerda por 2,5 segundos
+    }
+  }
+  else if (abs(dif) < 10)
+  {
+    if (dif < 0)
+    {
+      ajusta_para(true, 0.2); // Para direita por 2,5 segundos
+    }
+    else
+    {
+      ajusta_para(false, 0.2); // Ajusta para esquerda por 2,5 segundos
     }
   }
 }
@@ -101,7 +155,7 @@ void loop()
 void pega_pontos()
 {
   le_gps(true, ponto0[0], ponto0[1]);
-  anda_para_frente(2500);
+  anda_para_frente(3000);
   delay(200);
   le_gps(true, ponto1[0], ponto1[1]);
 }
@@ -154,6 +208,11 @@ double modulo(double x, double y)
 double to_rad(double angulo_grau)
 {
   return (angulo_grau * PI / 180);
+}
+/*Conversor de Radiano para grau*/
+double to_ang(double angulo_rad)
+{
+  return angulo_rad * 90 / PI;
 }
 
 //////////////FUNÇÕES DE MOVIMENTAÇÃO////////////////////////
@@ -210,24 +269,24 @@ void gira_para_direita(int tempo)
   analogWrite(in2, 0);
   analogWrite(in4, 0);
 }
-
-void ajusta_para(bool direita)
+/*Se verdadeiro vai para a direita. Tempo colocado em segundos*/
+void ajusta_para(bool direita, float tempo)
 {
   if (direita)
   {
-    gira_para_direita(1000);
+    gira_para_direita(1000 * tempo);
     delay(200);
-    anda_para_frente(5000);
+    anda_para_frente(3000);
   }
   else
   {
-    gira_para_esquerda(1000);
+    gira_para_esquerda(1000 * tempo);
     delay(200);
-    anda_para_frente(5000);
+    anda_para_frente(3000);
   }
   Serial_Debug.println("Posição ajustada");
 }
-
+/*Para todos os motores*/
 void stop()
 {
   analogWrite(in1, 0);
